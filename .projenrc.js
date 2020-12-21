a0bf2720e0e36e40b5aefe2474a4d12f2b6a5a89
wchaws <wch@amazon.com>
2020-12-21 22:35:50 +0800

    init

---
+++ .projenrc.js
@@ -0,0 +1,99 @@
+const { TypeScriptProject } = require('projen');
+
+const project = new TypeScriptProject({
+  name: 'cdk-bootstrapless-synthesizer',
+
+  /* NodeProjectCommonOptions */
+  // allowLibraryDependencies: true,                                           /* Allow the project to include `peerDependencies` and `bundledDependencies`. */
+  // antitamper: true,                                                         /* Checks that after build there are no modified files on git. */
+  // autoDetectBin: true,                                                      /* Automatically add all executables under the `bin` directory to your `package.json` file under the `bin` section. */
+  // bin: undefined,                                                           /* Binary programs vended with your module. */
+  // buildWorkflow: true,                                                      /* Define a GitHub workflow for building PRs. */
+  // bundledDeps: undefined,                                                   /* List of dependencies to bundle into this module. */
+  // codeCov: false,                                                           /* Define a GitHub workflow step for sending code coverage metrics to https://codecov.io/ Uses codecov/codecov-action@v1 A secret is required for private repos. Configured with @codeCovTokenSecret. */
+  // codeCovTokenSecret: undefined,                                            /* Define the secret name for a specified https://codecov.io/ token A secret is required to send coverage for private repositories. */
+  // copyrightOwner: undefined,                                                /* License copyright owner. */
+  // copyrightPeriod: undefined,                                               /* The copyright years to put in the LICENSE file. */
+  // defaultReleaseBranch: 'master',                                           /* The name of the main release branch. */
+  // dependabot: true,                                                         /* Include dependabot configuration. */
+  // dependabotOptions: undefined,                                             /* Options for dependabot. */
+  deps: [
+    '@aws-cdk/cloud-assembly-schema',
+    '@aws-cdk/cx-api',
+    '@aws-cdk/core',
+  ], /* Runtime dependencies of this module. */
+  // devDeps: [],                                                              /* Build dependencies for this module. */
+  // entrypoint: 'lib/index.js',                                               /* Module entrypoint (`main` in `package.json`). */
+  // keywords: undefined,                                                      /* Keywords to include in `package.json`. */
+  // libdir: 'lib',                                                            /* Compiler artifacts output directory. */
+  // maxNodeVersion: undefined,                                                /* Minimum node.js version to require via `engines` (inclusive). */
+  // mergify: true,                                                            /* Adds mergify configuration. */
+  // mergifyAutoMergeLabel: 'auto-merge',                                      /* Automatically merge PRs that build successfully and have this label. */
+  // mergifyOptions: undefined,                                                /* Options for mergify. */
+  // minNodeVersion: undefined,                                                /* Minimum Node.js version to require via package.json `engines` (inclusive). */
+  // npmDistTag: 'latest',                                                     /* The dist-tag to use when releasing to npm. */
+  // npmignore: undefined,                                                     /* Additional entries to .npmignore. */
+  // npmignoreEnabled: true,                                                   /* Defines an .npmignore file. Normally this is only needed for libraries that are packaged as tarballs. */
+  // npmRegistry: 'registry.npmjs.org',                                        /* The registry url to use when releasing packages. */
+  // npmTaskExecution: NpmTaskExecution.PROJEN,                                /* Determines how tasks are executed when invoked as npm scripts (yarn/npm run xyz). */
+  // packageManager: NodePackageManager.YARN,                                  /* The Node Package Manager used to execute scripts. */
+  // peerDependencyOptions: undefined,                                         /* Options for `peerDeps`. */
+  // peerDeps: [],                                                             /* Peer dependencies for this module. */
+  // projenCommand: 'npx projen',                                              /* The shell command to use in order to run the projen CLI. */
+  // projenDevDependency: true,                                                /* Indicates of "projen" should be installed as a devDependency. */
+  // projenUpgradeAutoMerge: undefined,                                        /* Automatically merge projen upgrade PRs when build passes. */
+  // projenUpgradeSchedule: [ '0 6 * * *' ],                                   /* Customize the projenUpgrade schedule in cron expression. */
+  // projenUpgradeSecret: undefined,                                           /* Periodically submits a pull request for projen upgrades (executes `yarn projen:upgrade`). */
+  // projenVersion: Semver.latest(),                                           /* Version of projen to install. */
+  // pullRequestTemplate: true,                                                /* Include a GitHub pull request template. */
+  // pullRequestTemplateContents: undefined,                                   /* The contents of the pull request template. */
+  // rebuildBot: true,                                                         /* Installs a GitHub workflow which is triggered when the comment "@projen rebuild" is added to a pull request. */
+  // rebuildBotCommand: 'rebuild',                                             /* The pull request bot command to use in order to trigger a rebuild and commit of the contents of the branch. */
+  // releaseBranches: [ 'master' ],                                            /* Branches which trigger a release. */
+  // releaseEveryCommit: true,                                                 /* Automatically release new versions every commit to one of branches in `releaseBranches`. */
+  // releaseSchedule: undefined,                                               /* CRON schedule to trigger new releases. */
+  // releaseToNpm: false,                                                      /* Automatically release to npm when new versions are introduced. */
+  // releaseWorkflow: true,                                                    /* Define a GitHub workflow for releasing from "master" when new versions are bumped. */
+  // scripts: {},                                                              /* npm scripts to include. */
+  // srcdir: 'src',                                                            /* Typescript sources directory. */
+  // testdir: 'test',                                                          /* Tests directory. */
+  // workflowBootstrapSteps: 'yarn install --frozen-lockfile && yarn projen',  /* Workflow steps to use in order to bootstrap this repo. */
+  // workflowContainerImage: undefined,                                        /* Container image to use for GitHub workflows. */
+  // workflowNodeVersion: undefined,                                           /* The node version to use in GitHub workflows. */
+
+  /* NodeProjectOptions */
+  // authorEmail: undefined,                                                   /* Author's e-mail. */
+  // authorName: undefined,                                                    /* Author's name. */
+  // authorOrganization: undefined,                                            /* Author's Organization. */
+  // authorUrl: undefined,                                                     /* Author's URL / Website. */
+  // description: undefined,                                                   /* The description is just a string that helps people understand the purpose of the package. */
+  // gitignore: undefined,                                                     /* Additional entries to .gitignore. */
+  // homepage: undefined,                                                      /* Package's Homepage / Website. */
+  // jest: true,                                                               /* Setup jest unit tests. */
+  // jestOptions: undefined,                                                   /* Jest options. */
+  // license: undefined,                                                       /* License's SPDX identifier. */
+  // licensed: true,                                                           /* Indicates if a license should be added. */
+  // repository: undefined,                                                    /* The repository is the location where the actual code for your package lives. */
+  // repositoryDirectory: undefined,                                           /* If the package.json for your package is not in the root directory (for example if it is part of a monorepo), you can specify the directory in which it lives. */
+  // stability: undefined,                                                     /* Package's Stability. */
+
+  /* ProjectOptions */
+  // gitpod: false,                                                            /* Adds a gitpod configuration. */
+  // outdir: '.',                                                              /* The root directory of the project. */
+  // parent: undefined,                                                        /* The parent project, if this project is part of a bigger project. */
+
+  /* TypeScriptProjectOptions */
+  // compileBeforeTest: undefined,                                             /* Compile the code before running tests. */
+  // disableTsconfig: false,                                                   /* Do not generate a `tsconfig.json` file (used by jsii projects since tsconfig.json is generated by the jsii compiler). */
+  // docgen: false,                                                            /* Docgen by Typedoc. */
+  // docsDirectory: 'docs',                                                    /* Docs directory. */
+  // entrypointTypes: undefined,                                               /* The .d.ts file that includes the type declarations for this module. */
+  // eslint: true,                                                             /* Setup eslint. */
+  // eslintOptions: undefined,                                                 /* Eslint options. */
+  // package: true,                                                            /* Defines a `yarn package` command that will produce a tarball and place it under `dist/js`. */
+  // sampleCode: true,                                                         /* Generate one-time sample in `src/` and `test/` if there are no files there. */
+  // tsconfig: undefined,                                                      /* Custom TSConfig. */
+  // typescriptVersion: '^3.9.5',                                              /* TypeScript version to use. */
+});
+
+project.synth();
---
+++ src/index.ts
@@ -0,0 +1,329 @@
+import * as fs from 'fs';
+import * as path from 'path';
+// import * as crypto from 'crypto';
+import * as cxschema from '@aws-cdk/cloud-assembly-schema';
+import { DockerImageAssetLocation, DockerImageAssetSource, FileAssetLocation, FileAssetPackaging, FileAssetSource, Fn, ISynthesisSession, Stack, StackSynthesizer, Token } from '@aws-cdk/core';
+import * as cxapi from '@aws-cdk/cx-api';
+
+export type Region =
+  'us-east-2' |
+  'us-east-1' |
+  'us-west-1' |
+  'us-west-2' |
+  'ap-south-1' |
+  'ap-east-1' |
+  'ap-northeast-2' |
+  'ap-southeast-1' |
+  'ap-southeast-2' |
+  'ap-northeast-1' |
+  'ca-central-1' |
+  'cn-north-1' |
+  'cn-northwest-1' |
+  'eu-central-1' |
+  'eu-west-1' |
+  'eu-west-2' |
+  'eu-west-3' |
+  'eu-north-1' |
+  'me-south-1' |
+  'sa-east-1';
+
+export interface BootstraplessStackSynthesizerProps {
+  readonly fileAssetsBucketName?: string;
+  readonly imageAssetsRepositoryName?: string;
+
+  readonly fileAssetPublishingRoleArn?: string;
+  readonly imageAssetPublishingRoleArn?: string;
+
+  readonly fileAssetsPrefix?: string;
+  readonly fileAssetsRegionSet?: Region[];
+  readonly templateBucketName?: string;
+}
+
+export class BootstraplessStackSynthesizer extends StackSynthesizer {
+  /**
+   * Default file asset prefix
+   */
+  public static readonly DEFAULT_FILE_ASSET_PREFIX = '';
+
+  private _stack?: Stack;
+  private bucketName: string;
+  private repositoryName: string;
+  private fileAssetPublishingRoleArn?: string;
+  private imageAssetPublishingRoleArn?: string;
+  private fileAssetsPrefix?: string
+
+  private readonly files: NonNullable<cxschema.AssetManifest['files']> = {};
+  private readonly dockerImages: NonNullable<cxschema.AssetManifest['dockerImages']> = {};
+
+  constructor(private readonly props: BootstraplessStackSynthesizerProps = {}) {
+    super();
+  }
+
+  public bind(stack: Stack): void {
+    if (this._stack !== undefined) {
+      throw new Error('A StackSynthesizer can only be used for one Stack: create a new instance to use with a different Stack');
+    }
+
+    this._stack = stack;
+
+    // Function to replace placeholders in the input string as much as possible
+    //
+    // We replace:
+    // - ${AWS::AccountId}, ${AWS::Region}: only if we have the actual values available
+    // - ${AWS::Partition}: never, since we never have the actual partition value.
+    const specialize = (s: string) => {
+      return cxapi.EnvironmentPlaceholders.replace(s, {
+        region: resolvedOr(stack.region, cxapi.EnvironmentPlaceholders.CURRENT_REGION),
+        accountId: resolvedOr(stack.account, cxapi.EnvironmentPlaceholders.CURRENT_ACCOUNT),
+        partition: cxapi.EnvironmentPlaceholders.CURRENT_PARTITION,
+      });
+    };
+
+    /* eslint-disable max-len */
+    this.bucketName = specialize(this.props.fileAssetsBucketName ?? '');
+    this.repositoryName = specialize(this.props.imageAssetsRepositoryName ?? '');
+    this.fileAssetPublishingRoleArn = this.props.fileAssetPublishingRoleArn ? specialize(this.props.fileAssetPublishingRoleArn) : undefined;
+    this.imageAssetPublishingRoleArn = this.props.imageAssetPublishingRoleArn ? specialize(this.props.imageAssetPublishingRoleArn) : undefined;
+    this.fileAssetsPrefix = specialize(this.props.fileAssetsPrefix ?? BootstraplessStackSynthesizer.DEFAULT_FILE_ASSET_PREFIX);
+    /* eslint-enable max-len */
+  }
+
+  public addFileAsset(asset: FileAssetSource): FileAssetLocation {
+    return this._addFileAsset(asset);
+  }
+
+  private _addFileAsset(asset: FileAssetSource, overrideBucketname?: string): FileAssetLocation {
+    assertBound(this.stack);
+    assertBound(this.bucketName);
+
+    const bucketName = overrideBucketname ?? this.bucketName;
+    const objectKey = this.fileAssetsPrefix + asset.sourceHash + (asset.packaging === FileAssetPackaging.ZIP_DIRECTORY ? '.zip' : '');
+    const destinations: { [id: string]: cxschema.FileDestination } = {};
+
+    if (this.props.fileAssetsRegionSet && this.props.fileAssetsRegionSet.length && bucketName.includes('${AWS::Region}')) {
+      for (let region of this.props.fileAssetsRegionSet) {
+        destinations[region] = {
+          bucketName: replaceAll(bucketName, '${AWS::Region}', region),
+          objectKey,
+          region,
+          assumeRoleArn: this.fileAssetPublishingRoleArn,
+        };
+      }
+    } else {
+      destinations[this.manifestEnvName] = {
+        bucketName,
+        objectKey,
+        region: resolvedOr(this.stack.region, undefined),
+        assumeRoleArn: this.fileAssetPublishingRoleArn,
+      };
+    }
+
+    // Add to manifest
+    this.files[asset.sourceHash] = {
+      source: {
+        path: asset.fileName,
+        packaging: asset.packaging,
+      },
+      destinations,
+    };
+
+    const { region, urlSuffix } = stackLocationOrInstrinsics(this.stack);
+    const httpUrl = cfnify(`https://s3.${region}.${urlSuffix}/${bucketName}/${objectKey}`);
+    const s3ObjectUrl = cfnify(`s3://${bucketName}/${objectKey}`);
+
+    // Return CFN expression
+    return {
+      bucketName: cfnify(bucketName),
+      objectKey,
+      httpUrl,
+      s3ObjectUrl,
+      s3Url: httpUrl,
+    };
+  }
+
+  public addDockerImageAsset(asset: DockerImageAssetSource): DockerImageAssetLocation {
+    assertBound(this.stack);
+    assertBound(this.repositoryName);
+
+    const imageTag = asset.sourceHash;
+
+    // Add to manifest
+    this.dockerImages[asset.sourceHash] = {
+      source: {
+        directory: asset.directoryName,
+        dockerBuildArgs: asset.dockerBuildArgs,
+        dockerBuildTarget: asset.dockerBuildTarget,
+        dockerFile: asset.dockerFile,
+      },
+      destinations: {
+        [this.manifestEnvName]: {
+          repositoryName: this.repositoryName,
+          imageTag,
+          region: resolvedOr(this.stack.region, undefined),
+          assumeRoleArn: this.imageAssetPublishingRoleArn,
+        },
+      },
+    };
+
+    const { account, region, urlSuffix } = stackLocationOrInstrinsics(this.stack);
+
+    // Return CFN expression
+    return {
+      repositoryName: cfnify(this.repositoryName),
+      imageUri: cfnify(`${account}.dkr.ecr.${region}.${urlSuffix}/${this.repositoryName}:${imageTag}`),
+    };
+  }
+
+  /**
+   * Synthesize the associated stack to the session
+   */
+  public synthesize(session: ISynthesisSession): void {
+    assertBound(this.stack);
+
+    this.synthesizeStackTemplate(this.stack, session);
+
+    // Add the stack's template to the artifact manifest
+    const templateManifestUrl = this.addStackTemplateToAssetManifest(session);
+
+    const artifactId = this.writeAssetManifest(session);
+
+    this.emitStackArtifact(this.stack, session, {
+      stackTemplateAssetObjectUrl: templateManifestUrl,
+      additionalDependencies: [artifactId],
+    });
+  }
+
+  protected get stack(): Stack | undefined {
+    return this._stack;
+  }
+
+  /**
+   * Add the stack's template as one of the manifest assets
+   *
+   * This will make it get uploaded to S3 automatically by S3-assets. Return
+   * the manifest URL.
+   *
+   * (We can't return the location returned from `addFileAsset`, as that
+   * contains CloudFormation intrinsics which can't go into the manifest).
+   */
+  private addStackTemplateToAssetManifest(_: ISynthesisSession) {
+    assertBound(this.stack);
+
+    const sourceHash = this.stack.templateFile;
+
+    this._addFileAsset({
+      fileName: this.stack.templateFile,
+      packaging: FileAssetPackaging.FILE,
+      sourceHash,
+    }, this.props.templateBucketName);
+
+    // We should technically return an 'https://s3.REGION.amazonaws.com[.cn]/name/hash' URL here,
+    // because that is what CloudFormation expects to see.
+    //
+    // However, there's no way for us to actually know the UrlSuffix a priori, so we can't construct it here.
+    //
+    // Instead, we'll have a protocol with the CLI that we put an 's3://.../...' URL here, and the CLI
+    // is going to resolve it to the correct 'https://.../' URL before it gives it to CloudFormation.
+    return `s3://${this.bucketName}/${sourceHash}`;
+  }
+
+  /**
+   * Write an asset manifest to the Cloud Assembly, return the artifact IDs written
+   */
+  private writeAssetManifest(session: ISynthesisSession): string {
+    assertBound(this.stack);
+
+    const artifactId = `${this.stack.artifactId}.assets`;
+    const manifestFile = `${artifactId}.json`;
+    const outPath = path.join(session.assembly.outdir, manifestFile);
+
+    const manifest: cxschema.AssetManifest = {
+      version: cxschema.Manifest.version(),
+      files: this.files,
+      dockerImages: this.dockerImages,
+    };
+
+    fs.writeFileSync(outPath, JSON.stringify(manifest, undefined, 2));
+    session.assembly.addArtifact(artifactId, {
+      type: cxschema.ArtifactType.ASSET_MANIFEST,
+      properties: {
+        file: manifestFile,
+      },
+    });
+
+    return artifactId;
+  }
+
+  private get manifestEnvName(): string {
+    assertBound(this.stack);
+
+    return [
+      resolvedOr(this.stack.account, 'current_account'),
+      resolvedOr(this.stack.region, 'current_region'),
+    ].join('-');
+  }
+}
+
+/**
+ * Return the given value if resolved or fall back to a default
+ */
+function resolvedOr<A>(x: string, def: A): string | A {
+  return Token.isUnresolved(x) ? def : x;
+}
+
+/**
+ * A "replace-all" function that doesn't require us escaping a literal string to a regex
+ */
+function replaceAll(s: string, search: string, replace: string) {
+  return s.split(search).join(replace);
+}
+
+/**
+ * If the string still contains placeholders, wrap it in a Fn::Sub so they will be substituted at CFN deployment time
+ *
+ * (This happens to work because the placeholders we picked map directly onto CFN
+ * placeholders. If they didn't we'd have to do a transformation here).
+ */
+function cfnify(s: string): string {
+  return s.indexOf('${') > -1 ? Fn.sub(s) : s;
+}
+
+/**
+ * Return the stack locations if they're concrete, or the original CFN intrisics otherwise
+ *
+ * We need to return these instead of the tokenized versions of the strings,
+ * since we must accept those same ${AWS::AccountId}/${AWS::Region} placeholders
+ * in bucket names and role names (in order to allow environment-agnostic stacks).
+ *
+ * We'll wrap a single {Fn::Sub} around the final string in order to replace everything,
+ * but we can't have the token system render part of the string to {Fn::Join} because
+ * the CFN specification doesn't allow the {Fn::Sub} template string to be an arbitrary
+ * expression--it must be a string literal.
+ */
+function stackLocationOrInstrinsics(stack: Stack) {
+  return {
+    account: resolvedOr(stack.account, '${AWS::AccountId}'),
+    region: resolvedOr(stack.region, '${AWS::Region}'),
+    urlSuffix: resolvedOr(stack.urlSuffix, '${AWS::URLSuffix}'),
+  };
+}
+
+
+// function range(startIncl: number, endExcl: number) {
+//     const ret = new Array<number>();
+//     for (let i = startIncl; i < endExcl; i++) {
+//     ret.push(i);
+//     }
+//     return ret;
+// }
+
+
+function assertBound<A>(x: A | undefined): asserts x is NonNullable<A> {
+  if (x === null && x === undefined) {
+    throw new Error('You must call bindStack() first');
+  }
+}
+
+// function contentHash(content: string) {
+//   return crypto.createHash('sha256').update(content).digest('hex');
+// }
